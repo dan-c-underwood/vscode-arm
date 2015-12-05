7b3c042ce1079fb8cc1503f84f6c9fc4c222464f
Dan Underwood <dan.underwood@outlook.com>
2015-12-05 20:09:22 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# ARM assembly highlighting for Visual Studio Code
+
+Adds syntax highlighting for the ARM Assembly language to Visual Studio Code. This extension was originally written to support the ARM instruction set used in teaching at the University of Manchester so availability of instructions on platforms is not guaranteed.
+
+The extension is a converted version of the syntax highlighting package for the Atom editor.
+
+## Install
+
+First install [Visual Studio Code](https://code.visualstudio.com). In the command palette (`cmd-shift-p`) select `Install Extension` and choose `ARM`.  
+
+## License
+[MIT](LICENSE)
---
+++ test.s
@@ -0,0 +1,40 @@
+@	test.s
+@	dan-c-underwood
+@	Version - 1.0.0
+@	15/08/15
+@
+@	Description - Example ARM Source File
+
+strings:
+string_a: 	defw	"Awaiting input"
+string_b:	defb    'a', 'b', 'c', 'd'
+
+branches:
+		b	start			@ With comment!
+		bne     end
+		bl      linked_place
+
+		include test.s
+
+stack:
+stack_end:      defs	512			@ Stack Declaration
+
+
+		/* C Style comments
+		Can be multiline */
+start:
+		adr	r6, stack_end
+		mov	r0, #&3f
+		bic	r2, r0
+		mov	r1, sp
+		strb	r0, [r1, #stack]
+		nop
+		stmfd	sp!, {r0-r2, lr}
+		movs	r2, r2, lsr #1
+		cmp	r0, r2
+		addnes	r0, r0, #&1
+		bne	start
+		ldmfd	sp!, {r0-r2, pc}^
+
+end:
+		svc	0

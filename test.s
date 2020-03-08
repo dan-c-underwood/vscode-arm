e0ab504d7f49ff6d2c97e7d558f35906e3f3a868
Dan Underwood <dan.underwood@outlook.com>
2020-03-08 11:22:07 +0000

    Update test assembly file

--- test.s
+++ test.s
@@ -1,49 +1,43 @@
 @	test.s
 @	dan-c-underwood
-@	Version - 1.1.0
+@	Version - 1.2.0
 @	15/08/15
 @
 @	Description - Example ARM Source File
 
 strings:
-string_a: 		defw	"Awaiting input"
-string_b:		defb    'a', 'b', 'c', 'd'
+string_a: 	defw	"Awaiting input"
+string_b:	defb    'a', 'b', 'c', 'd'
 
 branches:
 		b		start			@ With comment!
 		bne     end
 		bl      linked_place
 
-		.include "test.s"
-
-
-#if defined(TEST)
-	nop
-#endif
-
-#if ARM_ASSEMBLY
-	svc 0
-#endif
+		include test.s
 
 stack:
-stack_end:      defs	512		@ Stack Declaration
+stack_end:      defs	512			@ Stack Declaration
 
 
 		/* C Style comments
 		Can be multiline */
 start:
-		adr		r6, stack_end
-		mov		r0, #&3f
-		bic		r2, r0
-		mov		r1, sp
+		adr	r6, stack_end
+		mov	r0, #&3f
+		bic	r2, r0
+		mov	r1, sp
 		strb	r0, [r1, #stack]
 		nop
 		stmfd	sp!, {r0-r2, lr}
 		movs	r2, r2, lsr #1
-		cmp		r0, r2
-		adds	r0, r0, #&1
-		bne		start
+		cmp	r0, r2
+		addnes	r0, r0, #&1
+		bne	start
 		ldmfd	sp!, {r0-r2, pc}^
+		ldreq	x16, r1, [r2, #0x80]
+		pacdzb	x16
+		retab
 
 end:
 		svc	0
